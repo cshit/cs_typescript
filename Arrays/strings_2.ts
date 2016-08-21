151f034c30c37b0f7c0bb245fcb22911db528fc4
sebabelmar <seba.belmar@devbootcamp.com>
2016-08-20 21:09:23 -0700

    Strings problem 2. 2 solutions

---
+++ Arrays/strings_2.ts
@@ -0,0 +1,64 @@
+/*
+* Check Permutation
+*
+* Given two strings, write a method to decide if one is a permutation of the other.
+*
+* */
+
+
+// Sol 1. Sort and compare
+
+function sortString(str: string): string {
+	return str.split('').sort().join('');
+}
+
+function permutation(s: string, t:string): boolean{
+	return sortString(s) === sortString(t);
+}
+
+
+
+console.log(permutation("melon", "lemon"));
+
+
+// Sol 2. Check if both strings have the same character count
+// Return false if string's length id different
+// Get an array with 128 elements of 0 ~ [0,0,0,0,0...]
+// Split the first array and increment the count of the position of the character in the array
+// using its ASCII code
+// Iterate over the other string subtracting 1 to the character corresponding position
+// Return if any value of the array turns into a negative number
+
+
+function permutation2 (s: string, t: string): boolean{
+	// Return in case of an stupid case
+	if(s.length != t.length)
+		return false;
+
+	// Get the array of 128 zeros
+	var letters = Array(128).fill(0);
+
+	// Lets count the letters
+	// Split s
+	var sArray: number[] = s.split('').map(function(letter: string){return letter.charCodeAt()});
+
+	// Iterate over new array and count in array
+	for(var letterCode in sArray){
+		letters[sArray[letterCode]]++;
+	}
+	
+	// Look for negatives counts when substracting
+	for (var i: number = 0; i <= t.length; i++){
+		var c: number = t.charCodeAt(i);
+		letters[c]--;
+		if(letters[c] < 0){
+			return false
+		}
+	}
+	
+	
+	return true
+}
+
+
+console.log(permutation2('mellon', 'llemon'));
