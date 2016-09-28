e285ed5542d33a3de4c3cb4aa0b1a9f6c5564914
sebabelmar <seba.belmar@devbootcamp.com>
2016-09-28 11:34:47 -0700

    Added some preliminary comments on QuickSort

--- Sort/Quick/Quick.ts
+++ Sort/Quick/Quick.ts
@@ -13,22 +13,26 @@ function partition(arr, low, high):number
 {
 	var pivotIndex = Math.floor((low+high)/2);
 	
+	// Swaps pivot value with last element in the array's section
 	var temp1 = arr[pivotIndex];
 	arr[pivotIndex] = arr[high];
 	arr[high] = temp1;
 
 	var i:number = low;
-	for(let j:number=low; j < high; j++)
+	for(let j:number=low; j<high; j++)
 	{
-		if(arr[j] <= arr[high])
+		if(arr[j] < arr[high])
 		{
+			// Swaps any value higher than pivot to the left of it.
 			var temp2:number = arr[i];
 			arr[i] = arr[j];
 			arr[j] = temp2;
 			i++
 		}
 	}
-
+	
+	// Swaps the value at the end(pivot) with the following value of the
+	// swapped ones so pivot is to the right of all the lower ones
 	var temp3   = arr[i];
 	arr[i]      = arr[high];
 	arr[high]   = temp3;
