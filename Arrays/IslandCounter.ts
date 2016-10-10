82a2dfbfca5fe4835559b817a9ae849f54e4efb3
sebabelmar <seba.belmar@devbootcamp.com>
2016-10-09 21:33:24 -0700

    Counting Islands using map/recursive approach only one N^2.

--- Arrays/IslandCounter.ts
+++ Arrays/IslandCounter.ts
@@ -2,43 +2,36 @@ export default function countIslands(target:string[][]):number
 {
 	var count = 0;
 	var map = {};
-	
-	for(let y=0; y<target.length; y++)
-	{
-		const row = target[y];
-		
-		for(let x=0; x<row.length; x++)
-		{
-			map[`${x},${y}`] = row[x];
-		}
-	}
 
 	function checkForX(x:number, y:number, start?:boolean):boolean
 	{
-		const key       = `${x},${y}`;
-		const value     = map[key];
-
-		if(value)
+		if(x>=0 && y>=0 && y<target.length && x<target[y].length)
 		{
-			delete map[key];
+			const key       = `${x},${y}`;
+			const value     = map[key];
 
-			if(value == 'X')
+			if(!value)
 			{
+				map[key] = true;
 
-				if(!start)
+				if(target[y][x] == 'X')
 				{
-					checkForX(x, y-1);
-					checkForX(x-1, y);
+
+					checkForX(x+1, y);
+					if(!start)
+					{
+						checkForX(x, y-1);
+						checkForX(x-1, y);
+					}
+
+					checkForX(x, y+1);
+
+					return true;
 				}
 
-				checkForX(x+1, y);
-				checkForX(x, y+1);
-
-				return true;
 			}
 
 		}
-
 		return false;
 	}
 
